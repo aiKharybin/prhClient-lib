af2f891a6edf9d444031b636ae64e729982fc761
Kharybin <ai.kharybin@gmail.com>
2020-06-24 13:19:02 +0300

    api method comment

--- src/main/java/com/kharybin/prhclient/lib/PrhClient.java
+++ src/main/java/com/kharybin/prhclient/lib/PrhClient.java
@@ -2,6 +2,12 @@ package com.kharybin.prhclient.lib;
 
 import com.kharybin.prhclient.lib.model.CompanyData;
 
+
+
 public interface PrhClient {
+    /**
+     * Returns an {@link com.kharybin.prhclient.lib.model.CompanyData}
+     * takes String in ddddddd-d format as input
+     */
     CompanyData getCompanyDataById(String businessId);
 }
